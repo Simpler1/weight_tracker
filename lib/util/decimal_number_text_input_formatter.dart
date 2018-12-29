f4726583f906594e50469fa64eafa5e5ed006928
Simpler1 <kevin.bezold@gmail.com>
2018-12-28 23:19:09 -0500

    fix(decimal_number_text_input_formatter): Rework to format initial value

--- lib/util/decimal_number_text_input_formatter.dart
+++ lib/util/decimal_number_text_input_formatter.dart
@@ -3,39 +3,27 @@ import 'package:intl/intl.dart';
 import 'dart:math';
 
 class DecimalNumberTextInputFormatter extends TextInputFormatter {
-  int decimalPlaces;
-
   DecimalNumberTextInputFormatter({this.decimalPlaces = 2});
+
+  final int decimalPlaces;
+
+  String defaultFormat(String textIn) {
+    if (textIn == null) textIn = '';
+    String textOut = formattedNewValue(TextEditingValue(), TextEditingValue(text: textIn));
+    return textOut;
+  }
 
   @override
   TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
     // Check for no change
-    if (newValue.text.compareTo(oldValue.text) == 0) {
+    if (oldValue != null && newValue.text.compareTo(oldValue.text) == 0) {
       return oldValue;
     }
 
-    final String stripped = newValue.text.replaceAll(RegExp('\\D'), '');
+    String newString = formattedNewValue(oldValue, newValue);
 
-    // Check for zero
-    if (double.parse(stripped) == 0.0) {
-      return TextEditingValue(text: '');
-    }
-
-    final int intNum = int.tryParse(stripped);
-
-    // Check for blank field
-    if (intNum == null) {
-      return oldValue;
-    }
-
-    final double dblNum = intNum / pow(10, decimalPlaces);
-
-    final String places = List.filled(decimalPlaces, '0').join();
-    final currencyFormat = NumberFormat('#,##0.' + places, 'en_US');
-    final String newString = currencyFormat.format(dblNum);
-
-    var cursorLocationFromEnd;
-    if (oldValue.selection.end < 0) {
+    int cursorLocationFromEnd;
+    if (oldValue == null || oldValue.selection.end < 0) {
       cursorLocationFromEnd = 0;
     } else {
       cursorLocationFromEnd = oldValue.text.length - oldValue.selection.end;
@@ -48,5 +36,52 @@ class DecimalNumberTextInputFormatter extends TextInputFormatter {
         offset: cursorLocation,
       ),
     );
+  }
+
+  String formattedNewValue(TextEditingValue oldValue, TextEditingValue newValue) {
+    String _digitsAndDots = newValue.text.replaceAll(RegExp('((?!\d|\.).)*'), '');
+    if (_digitsAndDots == '') {
+      return '';
+    }
+
+    final int _lastDot = _digitsAndDots.lastIndexOf('.');
+    if (oldValue.text == '') { // Starting from blank
+      // Pad or truncate to desired number of decimalPlaces
+      if (_lastDot >= 0) {
+        final int _currentDecimalPlaces = newValue.text.length - 1 - _lastDot;
+        if (_currentDecimalPlaces < decimalPlaces) {
+          _digitsAndDots = _digitsAndDots + List.filled(decimalPlaces - _currentDecimalPlaces, '0').join();
+        } else if (_currentDecimalPlaces > decimalPlaces) {
+          _digitsAndDots = _digitsAndDots.substring(0, _digitsAndDots.length - (_currentDecimalPlaces - decimalPlaces));
+        }
+      } else {
+        _digitsAndDots = _digitsAndDots + List.filled(decimalPlaces, '0').join();
+      }
+    }
+
+    // Remove all non-digits
+    final String stripped = _digitsAndDots.replaceAll(RegExp('\\D'), '');
+
+    // Check for zero
+    if (double.tryParse(stripped) == 0.0) {
+      return '0';
+    }
+
+    final int intNum = int.tryParse(stripped);
+
+    // Check for blank field
+    if (intNum == null) {
+      return '';
+    }
+
+    final double dblNum = intNum / pow(10, decimalPlaces);
+
+    String showDecimal = this.decimalPlaces == 0 ? '' : '.';
+
+    final String places = List.filled(decimalPlaces, '0').join();
+    final currencyFormat = NumberFormat('#,##0' + showDecimal + places, 'en_US');
+    final String newString = currencyFormat.format(dblNum);
+
+    return newString;
   }
 }
